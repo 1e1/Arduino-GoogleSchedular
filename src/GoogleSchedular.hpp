c54d41508b55b8e390417cedc80afcf0b04ccbec
1e1 <2.71828183e0+git@gmail.com>
2024-11-19 22:18:22 +0100

    reduce network impact

--- src/GoogleSchedular.hpp
+++ src/GoogleSchedular.hpp
@@ -5,9 +5,15 @@
 
 
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClientSecureBearSSL.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClientSecureBearSSL.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+  #include <WiFiClientSecure.h>
+#endif
 #include <list>
 
 #include <ArduinoJson.h>
@@ -18,9 +24,9 @@
 #include "GoogleApiCalendar.hpp"
 
 
-#ifndef SCHEDULAR_NAME_SEPARATOR
-#define SCHEDULAR_NAME_SEPARATOR ','
-#endif
+//#ifndef SCHEDULAR_NAME_SEPARATOR
+//#define SCHEDULAR_NAME_SEPARATOR ','
+//#endif
 
 
 class GoogleSchedular : public GoogleApiCalendar {
@@ -34,7 +40,7 @@ class GoogleSchedular : public GoogleApiCalendar {
     - D: initializing Device=> has device_code
     - E: Failed => has Error
     */
-    enum State: uint8_t {
+    enum State {
         VOID          = B0000,
         INIT          = B0010,
         AUTHENTICATED = B0110,
@@ -48,89 +54,125 @@ class GoogleSchedular : public GoogleApiCalendar {
     GoogleSchedular(const String& clientId, const String& clientSecret, TimestampRFC3339Ntp& ts) : GoogleApiCalendar(clientId, clientSecret), _timestamp(ts), _expirationTimestamp(0), _state(State::VOID), _eventList() {}
 
 
-    const boolean hasFailed(void) const           { return this->_state == State::ERROR; }
-    const boolean isInitialized(void) const       { return this->_state == State::INIT;  }
-    const boolean isAuthenticatedOnly(void) const { return this->_state == State::AUTHENTICATED; }
-    const boolean isAuthenticated(void) const     { return this->_state & B0100; }
-    const boolean isLinked(void) const            { return this->_state == State::LINKED; }
+    const boolean hasFailed(void) const       { return _state == State::ERROR; }
+    const boolean isInitialized(void) const   { return _state == State::INIT;  }
+    const boolean isAuthenticated(void) const { return _state & B0100; }
+    const boolean isLinked(void) const        { return _state == State::LINKED; }
 
-    std::list<String>getEventList(void) const { return this->_eventList; }
+    std::list<String>getEventList(void) const { return _eventList; }
 
     const boolean hasExpired(void)
     {
-        return this->_expirationTimestamp < this->_timestamp.getTimestampUnix();
+        return _expirationTimestamp < _timestamp.getTimestampUnix();
     }
 
     void setCalendar(String calendarName)
     {
-        if (this->isAuthenticated()) {
+        if (_state & State::AUTHENTICATED) {
             JsonDocument doc;
-            const GoogleOAuth2::Response ret = this->getCalendars(doc);
+            const GoogleOAuth2::Response ret = getCalendars(doc);
 
             if (ret == GoogleOAuth2::OK) {
+                _state = State::AUTHENTICATED;
+
                 const JsonArray items = doc[F("items")].as<JsonArray>();
                 for (JsonObject item : items) {
                     if (calendarName.equals(item[F("summary")].as<String>())) {
-                        this->_calendarId = item[F("id")].as<String>();
-                        this->_state = State::LINKED;
-                        return;
+                        _calendarId = item[F("id")].as<String>();
+                        _state = State::LINKED;
+                        break;
                     }
                 }
             }
         }
     }
 
+    void startQuietRegistration()
+    {
+        String url;
+        startRegistration(url, _accessToken);
+        _state = State::VOID;
+    }
+
+    const String getQuietUserCode(void) {
+        if (_state == State::VOID) {
+            _state = State::INIT;
+            return _accessToken;
+        }
+
+        return "";
+    }
+
     void startRegistration(String& url, String& code)
     {
-        this->_state = State::VOID;
+        _state = State::VOID;
 
         const String scope = FPSTR(GoogleApiCalendar::SCOPE);
         JsonDocument doc;
-        const GoogleOAuth2::Response ret = this->requestDeviceAndUserCode(doc, scope);
+        const GoogleOAuth2::Response ret = requestDeviceAndUserCode(doc, scope);
 
         if (ret == GoogleOAuth2::OK) {
             url  = doc[F("verification_url")].as<String>();
             code = doc[F("user_code")].as<String>();
 
             const uint8_t interval = doc[F("interval")];
-            this->_calendarId = String(interval);
+            _calendarId = String(interval);
 
-            this->_setExpirationTimestamp(interval);
-            this->_state = State::INIT;
+            _setExpirationTimestamp(interval);
+            _state = State::INIT;
         }
     }
 
     void maintain(void)
     {
-        if (this->isAuthenticated()) {
-            this->maintainAuthorization(false);
-        } else if (this->isInitialized()) {
-            this->handleRegistration();
+        // already authenticated
+        if (isAuthenticated()) {
+            // renew authentication if needed
+            maintainAuthorization(false);
+        // waiting for a device limited validation
+        } else if (isInitialized()) {
+            // authenticate depending the remote server
+            handleRegistration();
+        // no running authentication process (prevent "null" value)
+        } else if (_refreshToken.length()>8) {
+            if (_accessToken.isEmpty()) {
+                // has been init with refreshToken only
+                maintainAuthorization(true);
+                // refresh_token is incorrect
+                if (_state == State::ERROR) {
+                    _refreshToken.remove(0);
+                // first authentication
+                } else {
+                    _state = State::AUTHENTICATED;
+                }
+            } else {
+                // has been startQuietRegistration()
+            }
         }
     }
 
 
     void handleRegistration(void)
     {
-        if (this->hasExpired()) {
+        if (_expirationTimestamp < _timestamp.getTimestampUnix()) {
             JsonDocument doc;
-            const GoogleOAuth2::Response ret = this->pollAuthorization(doc);
+            const GoogleOAuth2::Response ret = pollAuthorization(doc);
 
             switch (ret) {
                 case GoogleOAuth2::PENDING: {
-                    const uint8_t interval = this->_calendarId.toInt();
-                    this->_setExpirationTimestamp(interval);
+                    const uint8_t interval = _calendarId.toInt();
+                    _setExpirationTimestamp(interval);
                     break;
                 }
                 case GoogleOAuth2::OK: {
-                    const uint16_t expiresInSeconds = doc[F("expires_in")].as<uint16_t>();
-                    this->_setSecureExpirationTimestamp(expiresInSeconds);
-                    this->_state = State::AUTHENTICATED;
+                    const uint16_t expiresInSeconds = doc[F("expires_in")];
+                    _setSecureExpirationTimestamp(expiresInSeconds);
+                    _state = State::AUTHENTICATED;
                     break;
                 }
                 default: {
-                    this->_setExpirationTimestamp(0);
-                    this->_state = State::ERROR;
+                    _setExpirationTimestamp(0);
+                    _state = State::ERROR;
                 }
             }
         }
@@ -138,14 +180,14 @@ class GoogleSchedular : public GoogleApiCalendar {
 
     void maintainAuthorization(const boolean force=false)
     {
-        if (force || this->hasExpired()) {
+        if (force || hasExpired()) {
             JsonDocument doc;
-            const GoogleOAuth2::Response ret = this->refreshAccessToken(doc);
+            const GoogleOAuth2::Response ret = refreshAccessToken(doc);
             if (ret == GoogleOAuth2::OK) {
-                const uint16_t expiresInSeconds = doc[F("expires_in")].as<uint16_t>();
-                this->_setSecureExpirationTimestamp(expiresInSeconds);
+                const uint16_t expiresInSeconds = doc[F("expires_in")];
+                _setSecureExpirationTimestamp(expiresInSeconds);
             } else {
-                this->_state = State::ERROR;
+                _state = State::ERROR;
             }
         }
     }
@@ -160,15 +202,15 @@ class GoogleSchedular : public GoogleApiCalendar {
             t0.setCharAt(18, '0');
             ts.setCharAt(18, '9');
 
-            ret = this->getEvents(doc, this->_calendarId, t0, ts);
+            ret = getEvents(doc, _calendarId, t0, ts);
 
             ts.setCharAt(17, org);
         }
 
         if (ret == GoogleOAuth2::OK) {
-            this->_state = State::ERROR;
+            _state = State::ERROR;
         } else {
-            this->_eventList.clear();
+            _eventList.clear();
 
             const JsonArray items = doc[F("items")].as<JsonArray>();
 
@@ -176,7 +218,7 @@ class GoogleSchedular : public GoogleApiCalendar {
                 // String summary = item["summary"].as<String>();
                 // !!! valid by &fields=items(summary) only !!!
                 const String summary = item.begin()->value().as<String>();
-                this->_eventList.push_back(summary);
+                _eventList.push_back(summary);
                 /*
                 uint8_t startIndex = 0;
                 uint8_t endIndex;
@@ -184,7 +226,7 @@ class GoogleSchedular : public GoogleApiCalendar {
                     endIndex = summary.indexOf('\n', startIndex);
                     String line = summary.substring(startIndex, endIndex).trim();
 
-                    this->_eventList.push_back(line);
+                    _eventList.push_back(line);
                     startIndex = endIndex + 1;
                 } while (endIndex != -1);
                 */
@@ -197,12 +239,12 @@ class GoogleSchedular : public GoogleApiCalendar {
 
     void _setExpirationTimestamp(const uint16_t expiresInSeconds)
     {
-        this->_expirationTimestamp = this->_timestamp.getTimestampUnix(expiresInSeconds);
+        _expirationTimestamp = _timestamp.getTimestampUnix(expiresInSeconds);
     }
 
     void _setSecureExpirationTimestamp(const uint16_t expiresInSeconds)
     {
-        this->_expirationTimestamp = this->_timestamp.getTimestampUnix(expiresInSeconds - GoogleSchedular::EXPIRATION_TIME_MARGIN);
+        _expirationTimestamp = _timestamp.getTimestampUnix(expiresInSeconds - GoogleSchedular::EXPIRATION_TIME_MARGIN);
     }
 
     
